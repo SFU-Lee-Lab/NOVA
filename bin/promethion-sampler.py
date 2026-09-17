acec070249f4c5bd43b39f1696cb8dc929318bcd
jimmyliu1326 <jimmyliu1326@yahoo.com>
2026-09-17 01:32:53 -0700

    output samplesheet to reference source barcode dirs

--- bin/promethion-sampler.py
+++ bin/promethion-sampler.py
@@ -187,7 +187,7 @@ for old_dir, new_dir in dirs_to_process.items():
 
 # Create new sample sheet for input to Samnsero
 print(f"=> Saving new sample sheet to '{output_sheet}'")
-df2 = df1.copy()[["sample_id"]]
-df2["data_path"] = dir_pass + "/" + df2["sample_id"] + "/"
+df2 = df1.copy()[["sample_id", "barcode"]]
+df2["data_path"] = dir_pass + "/" + df2["barcode"].astype(str) + "/"
 df2.to_csv(os.path.join(output_sheet), header=False, index=False)
 print("=> Done!")
